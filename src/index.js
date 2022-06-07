897717c3679dd1001a8fce747dd19c07175bcae2
ovilogic <ovidiu.opreah@yahoo.com>
2022-06-07 23:04:41 +0100

    routing initialised

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home.js';
 
-import App from './App';
+import NoPage from './pages/NoPage';
 
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+          <Route index element={<Home />} />
+          
+         
+          <Route path="*" element={<NoPage />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
