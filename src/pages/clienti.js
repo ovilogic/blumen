31524ccabb0e86801e92b5893c7f77fe2260e6c7
ovilogic <ovidiu.opreah@yahoo.com>
2022-06-26 17:28:50 +0100

    improved responsive menu. clienti page set up and wired

---
+++ src/pages/clienti.js
@@ -0,0 +1,21 @@
+import FooterPanel from "../footerPanel";
+import NavPanel from "../navPanel";
+
+
+const Clients = () => {
+
+
+
+
+
+    return (
+        <div>
+            <NavPanel />
+            
+            <FooterPanel />
+        </div> 
+        
+     );
+}
+ 
+export default Clients;
